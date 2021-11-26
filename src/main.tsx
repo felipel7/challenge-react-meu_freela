4a971150a3005bed577293c0a09873435039f94d
felipel7 <felipelseven@gmail.com>
2021-11-26 17:56:03 -0300

    Initial Setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { render } from 'react-dom';
+import { App } from './App';
+import './styles/global.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const rootElement = document.getElementById('root');
+render(<App />, rootElement);
