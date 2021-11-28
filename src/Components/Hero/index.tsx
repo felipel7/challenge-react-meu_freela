b2bdc735fb46c0d97cb04bd983d7bc6fea7cf314
felipel7 <felipelseven@gmail.com>
2021-11-28 17:44:06 -0300

    chore: added responsiveness to Home page

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -3,14 +3,16 @@ import hero from '../../assets/images/hero.webp';
 
 const Hero = () => {
   return (
-    <section className={styles.heroContainer}>
+    <figure className={styles.heroContainer}>
       <img src={hero} alt='a man coding in the background' />
-      <div className={styles.heroHeading}>
+      <figcaption className={styles.heroHeading}>
         <p>Desenvolvedores prontos para ação</p>
         <h1>Seu projeto pronto na velocidade da luz</h1>
-        <button type='button'>Peça uma reunião</button>
-      </div>
-    </section>
+        <button type='button'>
+          <a href='#form'>Peça uma reunião</a>
+        </button>
+      </figcaption>
+    </figure>
   );
 };
 
