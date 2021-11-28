310d000a1aa1c892271fd6032d7d957238865f1e
felipel7 <felipelseven@gmail.com>
2021-11-28 19:25:00 -0300

    feat: added toastify

--- src/Components/Contact/index.tsx
+++ src/Components/Contact/index.tsx
@@ -1,22 +1,64 @@
+import { FormEvent, useRef, useState } from 'react';
+import { toast } from 'react-toastify';
 import styles from './styles.module.scss';
 
 const Contact = () => {
+  const [isValid, setIsValid] = useState(true);
+  const [errorPhoneMsg, setErrorPhoneMsg] = useState('');
+
+  const formRef = useRef<HTMLFormElement>(null);
+  const phoneRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const result = handleValidation(phoneRef.current!.value);
+
+    if (result) {
+      setIsValid(true);
+      toast.success('Mensagem enviada! :)');
+    } else {
+      setErrorPhoneMsg('Número inválido!');
+      setIsValid(false);
+      toast.error('Verifique os dados novamente!');
+    }
+
+    formRef.current?.reset();
+  };
+
+  const handleValidation = (value: string) => {
+    return /^\(?\d{2}\)? ?([2-9]|\d{9}) ?\d{4}-? ?\d{4}$/.test(value.trim());
+  };
+
   return (
-    <section className={styles.contactContainer}>
+    <section id='form' className={styles.contactContainer}>
       <h2>Mande um oi, ligamos para você!</h2>
       <p>Preencha seus dados para que a gente possa entrar em contato.</p>
 
-      <form>
+      <form ref={formRef} onSubmit={e => handleSubmit(e)}>
         <div className={styles.formControl}>
           <label htmlFor='name'>Nome Completo</label>
-          <input type='text' id='name' placeholder='Ex: Mateus Ávila Isidoro' />
+          <input
+            autoComplete='off'
+            type='text'
+            id='name'
+            placeholder='Ex: Mateus Ávila Isidoro'
+          />
         </div>
 
         <div className={styles.formControl}>
           <label htmlFor='phone'>Whatsapp</label>
-          <input type='text' id='phone' placeholder='(99) 99999-9999' />
+          <input
+            autoComplete='off'
+            className={isValid ? '' : `${styles.error}`}
+            id='phone'
+            placeholder='(99) 99999-9999'
+            ref={phoneRef}
+            type='text'
+          />
+          {!isValid && <span>{errorPhoneMsg}</span>}
         </div>
-        <button type='button'>Peça uma reunião</button>
+        <button type='submit'>Peça uma reunião</button>
       </form>
     </section>
   );
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,11 +1,30 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../../contexts/ThemeContext';
+import { ToastContainer } from 'react-toastify';
+
 import { Hero } from '../../Components/Hero';
 import { AboutUs } from '../../Components/AboutUs';
 import { Contact } from '../../Components/Contact';
 import { Footer } from '../../Components/Footer';
 
 const Home = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <>
+      <ToastContainer
+        position='top-right'
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme={theme === 'dark' ? 'dark' : 'light'}
+        style={{ fontSize: '16px' }}
+      />
       <main>
         <Hero />
         <AboutUs />
