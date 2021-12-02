b461b1a8edbec0107574f846bebe22e1f91aa875
felipel7 <felipelseven@gmail.com>
2021-12-01 22:16:06 -0300

    fix: header font-size

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -10,53 +10,51 @@ const Header = () => {
 
   return (
     <header className={styles.headerContainer}>
-      <nav className={styles.headerContent}>
-        <a href='/'>MeuFreela</a>
-        <ul className={isOpen ? `${styles.active}` : ''}>
-          <li>
-            <a
-              href='https://www.linkedin.com'
-              target='_blank'
-              title='linkedin profile'
-              rel='noreferrer'
-            >
-              <FaLinkedinIn size={32} />
-              <span>Linkedin</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.instagram.com'
-              target='_blank'
-              title='instagram profile'
-              rel='noreferrer'
-            >
-              <FaInstagram size={32} />
-              <span>Instagram</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.twitter.com'
-              target='_blank'
-              title='twitter profile'
-              rel='noreferrer'
-            >
-              <FaTwitter size={32} />
-              <span>Twitter</span>
-            </a>
-          </li>
-          <li className={styles.toggleTheme}>
-            <div>
-              <ThemeSetter />
-              <span>Trocar o tema</span>
-            </div>
-          </li>
-        </ul>
-        <span onClick={() => setIsOpen(!isOpen)}>
-          <Hamburger size={25} color={'#fff'} />
-        </span>
-      </nav>
+      <a href='/'>MeuFreela</a>
+      <ul className={isOpen ? `${styles.active}` : ''}>
+        <li>
+          <a
+            href='https://www.linkedin.com'
+            target='_blank'
+            title='linkedin profile'
+            rel='noreferrer noopener'
+          >
+            <FaLinkedinIn size={35} />
+            <span>Linkedin</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.instagram.com'
+            target='_blank'
+            title='instagram profile'
+            rel='noreferrer noopener'
+          >
+            <FaInstagram size={35} />
+            <span>Instagram</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.twitter.com'
+            target='_blank'
+            title='twitter profile'
+            rel='noreferrer noopener'
+          >
+            <FaTwitter size={35} />
+            <span>Twitter</span>
+          </a>
+        </li>
+        <li className={styles.toggleTheme}>
+          <div>
+            <ThemeSetter />
+            <span>Trocar o tema</span>
+          </div>
+        </li>
+      </ul>
+      <span onClick={() => setIsOpen(!isOpen)}>
+        <Hamburger size={25} color={'#fff'} />
+      </span>
     </header>
   );
 };
